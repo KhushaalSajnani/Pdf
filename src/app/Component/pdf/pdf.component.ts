bbe386aa6f9e987bdf558a93d6a9d2be2c507d65
khushaalSajnani <khushaalsajnani@gmail.com>
2021-06-05 01:57:59 +0530

    Initia Commit
    Only Query Params Task is left

---
+++ src/app/Component/pdf/pdf.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import printJS from "print-js";
+
+declare var require: any
+const FileSaver = require('file-saver');
+
+@Component({
+  selector: 'app-pdf',
+  template: `
+    <div class="container">
+
+      <div class="actions">
+        <button><fa-icon [icon]="['fas','search-plus']" id="plus" (click)="zoomMe()" class="fa"></fa-icon></button>
+        <button><fa-icon [icon]="['fas','search-minus']" id="minus" class="fa" (click)="zoomMe2()"></fa-icon></button>
+        <button><fa-icon [icon]="['fas','cloud-download-alt']" id="download" (click)="downMe()" class="fa"></fa-icon></button>
+        <button><fa-icon [icon]="['fas','print']" class="fa" (click)="printMe()" id="print"></fa-icon></button>
+        <button><fa-icon [icon]="['fas','sync-alt']" [spin]="true" id="rotate" (click)="rotateMe()" class="fa"></fa-icon></button>
+      </div>
+    </div>
+
+    <pdf-viewer [src]="pdf"
+                [render-text]="true" [rotation]="angle" [zoom]="zoom" [zoom]="zoom"></pdf-viewer>
+  `,
+  styleUrls: ['./pdf.component.css']
+})
+export class PdfComponent {
+  pdf = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf"
+  angle=0;
+  zoom=0;
+
+  rotateMe() {
+    this.angle+=90;
+  }
+
+  zoomMe() {
+    this.zoom+=0.5;
+  }
+
+  zoomMe2() {
+    this.zoom-=0.5;
+  }
+
+  printMe() {
+    printJS({printable:this.pdf, type:'pdf', showModal:true})
+  }
+
+  downMe() {
+    const pdfUrl = this.pdf;
+    const pdfName = 'Letter';
+    FileSaver.saveAs(pdfUrl, pdfName);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FontAwesomeModule , FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import { AppHeaderComponent } from './Component/app-header/app-header.component';
+import { ToolbarComponent } from './Component/toolbar/toolbar.component';
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import { PdfComponent } from './Component/pdf/pdf.component';
+import { PdfViewerModule} from "ng2-pdf-viewer";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    ToolbarComponent,
+    PdfComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FontAwesomeModule,PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
